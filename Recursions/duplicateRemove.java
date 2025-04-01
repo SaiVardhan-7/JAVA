ff07aa47a05726425a8eb29a8907a96422bef44e
SaiVardhan-7 <kattasaivardhan838@gmail.com>
2025-04-01 20:18:18 +0530

    Create duplicateRemove.java

---
+++ Recursions/duplicateRemove.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class duplicateRemove {
+    public static void remove(String str, int index){
+        if(index==str.length()){
+            return;
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string:");
+        String str = sc.nextLine();
+
+        remove(str,0);
+
+    }
+}
