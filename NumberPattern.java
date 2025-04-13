a3e0737f0fe63f6dacde0fb80bf42997fe320497
Hima Chandra Sai Vardhan Katta <kattasaivardhan838@gmail.com>
2025-04-13 18:38:52 +0530

    Add files via upload

---
+++ NumberPattern.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class NumberPattern {
+    public static void getNumberPattern(int n) {
+        for (int i = 0; i < 2 * n - 1; i++) {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                int top = i;
+                int bottom = j;
+                int left = (2 * n - 2) - j;
+                int right = (2 * n - 2) - i;
+                System.out.print(n - Math.min(Math.min(top, bottom), Math.min(left, right)) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of 'n' : ");
+        int n = sc.nextInt();
+        getNumberPattern(n);
+    }
+}
