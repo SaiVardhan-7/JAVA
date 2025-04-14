7b34a778ba566aa778cf76317333f1a316c10cd3
Hima Chandra Sai Vardhan Katta <kattasaivardhan838@gmail.com>
2025-04-14 12:24:38 +0530

    Update NumberPattern.java

--- NumberPattern.java
+++ NumberPattern.java
@@ -5,9 +5,9 @@ public class NumberPattern {
         for (int i = 0; i < 2 * n - 1; i++) {
             for (int j = 0; j < 2 * n - 1; j++) {
                 int top = i;
-                int bottom = j;
-                int left = (2 * n - 2) - j;
-                int right = (2 * n - 2) - i;
+                int left = j;
+                int right = (2 * n - 2) - j;
+                int bottom = (2 * n - 2) - i;
                 System.out.print(n - Math.min(Math.min(top, bottom), Math.min(left, right)) + " ");
             }
             System.out.println();
